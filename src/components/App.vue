a5121c3a7ebe9156e3551c9cd844b73c54dc0b74
Cezar Alexandre <cezaralexandredasilva@gmail.com>
2017-02-13 15:36:28 -0200

    Add App.vue

---
+++ src/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="app">
+		<header class="header">
+
+		</header>
+
+		<div class="content">
+			<router-view></router-view>
+		</div>
+		<footer class="footer">
+
+		</footer>
+	</div>
+</template>
+<script>
+
+export default {
+	name: 'App',
+	data(){
+		return {
+		}
+	}
+}
+</script>
